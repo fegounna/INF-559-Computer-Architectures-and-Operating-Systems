2c67b94e9c6eb6c3537c58e39eae7e1e0ec5888e
fegounna <yessin.moakher@polytechnique.edu>
2024-12-04 18:40:29 +0100

    attack lab

---
+++ TD 5 AttackLab/argument.d
@@ -0,0 +1,9 @@
+
+argument.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	48 c7 c7 32 1b ed 6b 	mov    $0x6bed1b32,%rdi
+   7:	c3                   	retq   
---
+++ TD 5 AttackLab/argument.s
@@ -0,0 +1,2 @@
+mov $0x6bed1b32, %rdi
+ret
